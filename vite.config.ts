b1606142663b0c6baec9833d6023e6ce2da29c2f
jasmjazz <house5324@gmail.com>
2024-02-03 15:52:54 +0800

    -- 調整檔案

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: './typescript-booking-website/',
   plugins: [
     vue(),
     vueJsx(),
